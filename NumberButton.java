ecb460b4e71a852b56602c18547e762a20351755
jlin492987 <46304810+jlin492987@users.noreply.github.com>
2025-07-07 14:37:10 -0500

    initial implementation

--- NumberButton.java
+++ NumberButton.java
@@ -1,1 +1,25 @@
 
+//import gui libraries
+import javax.swing.*;
+
+public class NumberButton {
+    private int value;
+    private int xCoord;
+    private int yCoord;
+    private JButton button;
+    public NumberButton(int value, int xCoord, int yCoord) {
+        this.value = value;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+
+        button = new JButton(String.valueOf(value));
+        button.setSize(100, 100);
+        button.setLocation(xCoord, yCoord);
+        button.setVisible(true);
+    }
+
+    public JButton getObject() {
+        return button;
+    }
+}
+
